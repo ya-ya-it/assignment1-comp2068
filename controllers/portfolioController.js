8c2650018a9a61297db8531a40f383ae5a4e67b6
ya-ya-it <ya-ya-it@yandex.com>
2018-06-15 19:04:47 -0400

    added framework

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,29 +1,34 @@
 exports.index = (req, res, next) => {
   res.render('index', {
     name: 'Home',
+    isActive: 'home',
   });
 };
 
 exports.about = (req, res, next) => {
   res.render('about-me', {
     name: 'About Me',
+    isActive: 'about',
   });
 };
 
 exports.projects = (req, res, next) => {
   res.render('projects', {
     name: 'Projects',
+    isActive: 'projects',
   });
 };
 
 exports.services = (req, res, next) => {
   res.render('services', {
     name: 'Services',
+    isActive: 'services',
   });
 };
 
 exports.contact = (req, res, next) => {
   res.render('contact-me', {
     name: 'Contact Me',
+    isActive: 'contact',
   });
 };
